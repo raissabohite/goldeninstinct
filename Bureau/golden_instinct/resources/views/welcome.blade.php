766b3a96359eeac66b160dc8d6c650948c72a4cd
raissabohite <bohiteahoua@gmail.com>
2024-10-17 20:04:16 +0000

    golden1

---
+++ Bureau/golden_instinct/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-blue-500 text-center">
+    j'ai mon sac dans la chambre et quelqu'un a fouiller dedans
+    Hello world!
+  </h1>
+</body>
+</html>
